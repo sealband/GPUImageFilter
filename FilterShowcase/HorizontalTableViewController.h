b4f8423e592773a05e0d4a95d4fbc21cbb810a2f
seal <zwzzebra@gmail.com>
2016-03-13 01:38:01 +0800

    add horizontal choose vc

---
+++ FilterShowcase/HorizontalTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HorizontalTableViewController.h
+//  FilterShowcase
+//
+//  Created by seal on 3/13/16.
+//  Copyright © 2016 Cell Phone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HorizontalTableViewController : UITableViewController
+
+@end
