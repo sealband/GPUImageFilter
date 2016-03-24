9d90205cbc8aa0801a5b5cc129e38517999aba77
seal <zwzzebra@gmail.com>
2016-03-24 11:44:34 +0800

    删除无用代码

--- FilterShowcase/SliderView.m
+++ FilterShowcase/SliderView.m
@@ -19,33 +19,14 @@
     return self;
 }
 
-//- (void)gestureRecognizer:(UIPanGestureRecognizer *)gesture
-//{
-//    CGPoint p = [gesture locationInView:self];
-//    float offset = (DEVICEW-320)/2;
-//    float x = MIN(MAX(offset, p.x),DEVICEW-offset);
-//    x = x-offset;
-//    float s = x/320.0;
-//    [sliderView setValue:(int)(sliderView.minimumValue+(sliderView.maximumValue-sliderView.minimumValue)*s)];
-//    
-//    if (gesture.state == UIGestureRecognizerStateEnded) {
-//        //        [self.delegate filterSlider:self valueChange:sliderView.value];
-//        [self sliderDidCancel:nil];
-//    }
-//}
-
 - (void)initUI
 {
-    
     mainFrame =  [[UIScreen mainScreen] bounds];
-
     
     self.backgroundColor = [UIColor whiteColor];
     
     self.frame = CGRectMake(CGRectGetMinX(self.frame), CGRectGetMinY(self.frame), CGRectGetWidth(self.frame), CGRectGetHeight(self.frame));
     float totalheight = CGRectGetHeight(self.frame);
-    //    float toolwidth = DEVICEW;
-    
     {
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
         btn.frame = CGRectMake(0, totalheight-47, 60, 44);
@@ -63,7 +44,6 @@
         [btn addTarget:self action:@selector(nextDidClick:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:btn];
     }
-    
     valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.frame.size.width/2-85, self.frame.size.height-40, 100, 25)];
     valueLabel.backgroundColor = [UIColor clearColor];
     valueLabel.font = [UIFont systemFontOfSize:15];
@@ -80,61 +60,16 @@
     [defaultValueLabel setTextAlignment:NSTextAlignmentCenter];
     [self addSubview:defaultValueLabel];
 
-
     float offsety = totalheight-50;
-//    [self addSubview:[CHLine lineWithFrame:CGRectMake(0, 0, DEVICEW, 0.5) color:[UIColor colorWithRed:193/255.0 green:193/255.0 blue:193/255.0 alpha:1]]];
-//    [self addSubview:[CHLine lineWithFrame:CGRectMake(0, offsety, DEVICEW, 0.5) color:LINECOLOR]];
-    
-//    UILabel *lab = [[UILabel alloc] initWithFrame:CGRectMake(0, offsety+2, self.frame.size.width, 49)];
-//    lab.text = @"";
-//    lab.font = F(18);
-//    lab.textAlignment = NSTextAlignmentCenter;
-//    lab.backgroundColor = [UIColor clearColor];
-//    lab.textColor = EDITORTEXTCOLOR;
-//    [self addSubview:lab];
-//    labTitel = lab;
-    
-//    {
-//        UILabel *lab = [[UILabel alloc] init];
-//        lab.textColor = [UIColor colorWithRed:118/255.0 green:118/255.0 blue:118/255.0 alpha:1];
-//        lab.font = F(13);
-//        lab.textAlignment = NSTextAlignmentCenter;
-//        lab.backgroundColor = [UIColor clearColor];
-//        [self addSubview:lab];
-//        labValue = lab;
-//    }
-    
-//    sliderView = [[SRTFilterSliderView alloc] initWithFrame:CGRectMake(35, (totalheight-44)/2.0-17.5, DEVICEW-70, 35)];
-//    [sliderView addTarget:self action:@selector(sliderValueDidChange:) forControlEvents:UIControlEventValueChanged];
-//    [sliderView addTarget:self action:@selector(sliderDidCancel:) forControlEvents:UIControlEventTouchDragExit];
-//    [self addSubview:sliderView];
     
     filterSlider = [[UISlider alloc] initWithFrame:CGRectMake(0, 10, self.frame.size.width, 30)];
     [filterSlider addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
     [filterSlider addTarget:self action:@selector(sliderDidCancel:) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:filterSlider];
-    
-    
-    
-    
-    
-//    CHLine *l = [[CHLine alloc] initWithFrame:CGRectMake(0,0,DEVICEW,totalheight-49)];
-//    l.backgroundColor = [UIColor clearColor];
-//    l.userInteractionEnabled = YES;
-//    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(gestureRecognizer:)];
-//    [l addGestureRecognizer:pan];
-//    [self addSubview:l];
 }
 
 - (void)dismiss
 {
-//    if ([[myDic getValueForKey:@"id"] intValue] == 5) {
-//        //模糊
-//        [circle removeFromSuperview];
-//        circle = nil;
-//        [blurView removeFromSuperview];
-//        blurView = nil;
-//    }
     [UIView animateWithDuration:0.2 animations:^{
         self.frame = CGRectMake(0, mainFrame.size.height, self.frame.size.width, self.frame.size.height);
     } completion:^(BOOL finished) {
@@ -144,8 +79,6 @@
 
 - (void)backDidClick:(id)sender
 {
-//    [self.delegate filterSlider:self valueChange:[[myDic getValueForKey:@"value"] intValue]];
-//    [self.delegate cancleCurrentFilter];
     [self.delegate filterSliderTag:senderTag senderValue:[[myDic getValueForKey:@"value"] floatValue]];
     [self dismiss];
 }
